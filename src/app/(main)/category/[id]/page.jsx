ab4f692545ee45b0af0df2aca487346b03bcb39e
SADIA <you@example.com>
2026-05-18 08:15:01 +0600

    Load News Data & Filter News Based On Category

---
+++ src/app/(main)/category/[id]/page.jsx
@@ -0,0 +1,46 @@
+import LeftSidebar from '@/components/homepage/news/LeftSidebar';
+import RightSidebar from '@/components/homepage/news/RightSidebar';
+import { getCategories, getNewsByCategoryId } from '@/lib/data';
+import React from 'react';
+
+
+const NewsCategoryPage = async ({ params }) => {
+    const { id } = await params;
+    console.log(id)
+
+    const categories = await getCategories()
+  // console.log(categories.news_category)
+
+
+  const news = await getNewsByCategoryId(id)
+//   console.log(news)
+
+    return (
+        <div className="grid grid-cols-12 gap-4 container mx-auto my-[60px]">
+            <div className="font-bold col-span-3">
+                <LeftSidebar categories={categories} activeId={id} />
+            </div>
+
+            <div className="font-bold col-span-6">
+                <h2 className='font-bold text-lg'>News by category</h2>
+                <div className="space-y-4 mt-6">
+                    {
+                      news.length > 0 ?  news.map(n => {
+                            return <div key={n._id} className="p-6 rounded-md border">
+                                {n.title}
+                            </div>
+                        }) : <h2 className='font-bold text-red-500 text-center text-2xl'>No News Found</h2>
+                    }
+                </div>
+            </div>
+
+            <div className=" col-span-3">
+                <RightSidebar />
+            </div>
+
+
+        </div>
+    );
+};
+
+export default NewsCategoryPage;
--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,31 +1,11 @@
-import LeftSidebar from "@/components/homepage/news/LeftSidebar";
-import RightSidebar from "@/components/homepage/news/RightSidebar";
-import Image from "next/image"
+import { redirect } from "next/navigation";
 
-const getCategories = async () => {
-  const res = await fetch("https://openapi.programming-hero.com/api/news/categories")
-  const data = await res.json()
-  return data.data;
-}
-
-export default async function Home() {
-  const categories = await getCategories()
-  console.log(categories.news_category)
+const default_category_id = "01"
+const Home = async () =>{
   return (
-    <div className="grid grid-cols-12 gap-4 container mx-auto my-[60px]">
-      <div className="font-bold col-span-3">
-       <LeftSidebar categories={categories} activeId={null}/>
-      </div>
-
-      <div className="font-bold text-3xl bg-purple-100 col-span-6">
-        All News
-      </div>
-
-      <div className=" col-span-3">
-         <RightSidebar/>
-      </div>
-
-
-    </div>
+    redirect(`/category/${default_category_id}`)
   );
 }
+
+
+export default Home;
